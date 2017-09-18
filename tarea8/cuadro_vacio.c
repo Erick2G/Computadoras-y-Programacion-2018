4e750d45fcb919cf3196677c1f9577847ac16b93
Erick2G <e2g369@gmail.com>
2017-09-18 12:59:05 -0500

    Add files via upload

---
+++ tarea8/cuadro_vacio.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main(){
+	int l=0;
+	int a=0;
+		for(a=1;a<=6;a=a+1){
+			printf("+ ");
+		}printf("\n");
+	for(l=1;l<=5;l=l+1){
+		for(a=1;a<=6;a=a+1){
+			if(a==1){printf("+ ");
+			}
+			if(a==6){printf("+");
+			}else{printf("  ");
+			}
+		}printf("\n");
+	}
+	for(a=1;a<=6;a=a+1){
+			printf("+ ");}
+}
---
+++ tarea8/tablanam.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main(){
+	printf("Tablas de multiplicar\n(Inserta los valores)\n");
+	int min=0;
+	int max=0;
+	int v=0;
+	printf("Desde la tabla del:");
+	scanf("%d",&min);
+	printf("Hasta la del:");
+	scanf("%d",&max);
+	if(min<max&&min!=max){
+		for(min;min<=max;min=min+1){
+			printf("\n");
+			for(v=1;v<=10;v=v+1){
+				printf("%dx%d = %d \n",min,v,min*v);
+			}
+		}
+	}else{ printf("\nLos numeros deben ser diferentes entre si\nEl primer valor debe ser menor que el segundo");
+	}
+}
