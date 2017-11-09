4df7a632625a6de86f6fff841258dd4307ad8145
Erick2G <e2g369@gmail.com>
2017-11-09 12:42:16 -0600

    Add files via upload

---
+++ tarea20/20_cuadro.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "iva.h"
+int main(int argc, char *argv[]){
+	int r=0;
+	int b=0;
+	int h=0;
+	printf("Cuadro de x\n  Altura: ");
+	scanf("%d",&r);
+	printf("  Largo:");
+	scanf("%d",&b);
+	h=fnDimension(b,r);
+	printf("%s",h);
+	
+	float c=0.0;
+	float result=0.0;
+	printf("\nIngrese una cantidad:");
+	scanf("%f",&c);
+	result=fniva(c);
+	printf("\n",result);
+}
