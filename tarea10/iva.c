69f72b3694253eefb58d8c7c05fe82e6b837da20
Erick2G <e2g369@gmail.com>
2017-11-09 12:36:33 -0600

    Delete iva.c

--- tarea10/iva.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main(){
-	int iva=0;
-	int cdd=0;
-	printf("IVA (En Mexico): 16 percent\nIngrese una cantidad:");
-	scanf("%d",&cdd);
-	iva=cdd*0.16;
-	printf("\nSubtotal: %d\nIVA de la cantidad: %d\nTotal:%d",cdd,iva,iva+cdd);
-}
